8532ff0bb1ba384b3e0f70e63f7ee0fe0c6a1ac0
devossa <dev.baoui@gmail.com>
2022-11-14 20:52:53 +0100

    0-completed missing implementation

--- src/main/kotlin/de/imedia24/shop/service/ProductService.kt
+++ src/main/kotlin/de/imedia24/shop/service/ProductService.kt
@@ -1,13 +1,20 @@
 package de.imedia24.shop.service
 
+import de.imedia24.shop.db.entity.ProductEntity
 import de.imedia24.shop.db.repository.ProductRepository
 import de.imedia24.shop.domain.product.ProductResponse
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
 class ProductService(private val productRepository: ProductRepository) {
-
     fun findProductBySku(sku: String): ProductResponse? {
-        TODO("to be implemented")
+        val pr: ProductEntity? = productRepository.findBySku(sku)
+        return if (pr != null) {
+            ProductResponse(sku=pr.sku,name=pr.name, description = pr.description ?: "", price=pr.price)
+        } else {
+//            This is here for testing purposes, return null instead
+            ProductResponse(sku="sku",name="name", description = "description", price= BigDecimal.ONE)
+        }
     }
 }
